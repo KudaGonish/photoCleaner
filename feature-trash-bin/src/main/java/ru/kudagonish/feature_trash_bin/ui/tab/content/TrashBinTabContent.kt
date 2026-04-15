dc9683a926b65bd637217e3ae5e6872ef38b90b9
p.riabovol <pavel_riabovol@mail.ru>
2026-04-15 15:50:40 +0300

    Добавлена тестовая реализация LazyVerticalStaggeredGrid

--- feature-trash-bin/src/main/java/ru/kudagonish/feature_trash_bin/ui/tab/content/TrashBinTabContent.kt
+++ feature-trash-bin/src/main/java/ru/kudagonish/feature_trash_bin/ui/tab/content/TrashBinTabContent.kt
@@ -1,12 +1,23 @@
 package ru.kudagonish.feature_trash_bin.ui.tab.content
 
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.staggeredgrid.LazyVerticalStaggeredGrid
+import androidx.compose.foundation.lazy.staggeredgrid.StaggeredGridCells
+import androidx.compose.foundation.lazy.staggeredgrid.items
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import kotlinx.collections.immutable.persistentListOf
+import ru.kudagonish.core_ui.elements.bottomMenuPadding
 import ru.kudagonish.core_ui.theme.PhotoCleanerTheme
 import ru.kudagonish.feature_trash_bin.ui.tab.ui.SingleChoiceSegmentedButtons
 import ru.kudagonish.feature_trash_bin.ui.tab.viewModel.TabType
@@ -27,6 +38,26 @@ internal fun TrashBinTabContent(
             tabState = state.currentTab,
             onTabClick = { sendEvent(Event.OnTabClick(it)) },
         )
+
+        LazyVerticalStaggeredGrid(
+            modifier = Modifier.fillMaxSize(),
+            verticalItemSpacing = 4.dp,
+            contentPadding = PaddingValues(
+                top = 16.dp,
+                bottom = bottomMenuPadding
+            ),
+            horizontalArrangement = Arrangement.spacedBy(4.dp),
+            columns = StaggeredGridCells.Fixed(2)
+        ) {
+            items(state.photos) { item ->
+                Text(
+                    "Item ${item.uri}",
+                    Modifier
+                        .border(1.dp, Color.Blue)
+                        .width(150.dp)
+                )
+            }
+        }
     }
 }
 
@@ -35,7 +66,10 @@ internal fun TrashBinTabContent(
 private fun TrashBinTabContentSystemTrashPreview() {
     PhotoCleanerTheme {
         TrashBinTabContent(
-            state = TrashBinScreenState(currentTab = TabType.TrashBin),
+            state = TrashBinScreenState(
+                currentTab = TabType.TrashBin,
+                photos = persistentListOf()
+            ),
             sendEvent = {}
         )
     }
@@ -46,7 +80,10 @@ private fun TrashBinTabContentSystemTrashPreview() {
 private fun TrashBinTabContentInstantPreview() {
     PhotoCleanerTheme {
         TrashBinTabContent(
-            state = TrashBinScreenState(currentTab = TabType.ToDeletion),
+            state = TrashBinScreenState(
+                currentTab = TabType.ToDeletion,
+                photos = persistentListOf()
+            ),
             sendEvent = {}
         )
     }
--- feature-trash-bin/src/main/java/ru/kudagonish/feature_trash_bin/ui/tab/viewModel/TrashBinScreenState.kt
+++ feature-trash-bin/src/main/java/ru/kudagonish/feature_trash_bin/ui/tab/viewModel/TrashBinScreenState.kt
@@ -1,10 +1,67 @@
 package ru.kudagonish.feature_trash_bin.ui.tab.viewModel
 
 import androidx.compose.runtime.Stable
+import kotlinx.collections.immutable.ImmutableList
+import kotlinx.collections.immutable.persistentListOf
 
 @Stable
 internal data class TrashBinScreenState(
-    val currentTab: TabType = TabType.TrashBin
+    val currentTab: TabType = TabType.TrashBin,
+    val photos: ImmutableList<PhotoInformation> = persistentListOf(
+        PhotoInformation("1"),
+        PhotoInformation("2"),
+        PhotoInformation("2"),
+        PhotoInformation("2"),
+        PhotoInformation("2"),
+        PhotoInformation("2"),
+        PhotoInformation("2"),
+        PhotoInformation("2"),
+        PhotoInformation("2"),
+        PhotoInformation("2"),
+        PhotoInformation("2"),
+        PhotoInformation("2"),
+        PhotoInformation("2"),
+        PhotoInformation("2"),
+        PhotoInformation("2"),
+        PhotoInformation("2"),
+        PhotoInformation("2"),
+        PhotoInformation("2"),
+        PhotoInformation("2"),
+        PhotoInformation("2"),
+        PhotoInformation("2"),
+        PhotoInformation("2"),
+        PhotoInformation("2"),
+        PhotoInformation("2"),
+        PhotoInformation("2"),
+        PhotoInformation("2"),
+        PhotoInformation("2"),
+        PhotoInformation("2"),
+        PhotoInformation("2"),
+        PhotoInformation("2"),
+        PhotoInformation("2"),
+        PhotoInformation("2"),
+        PhotoInformation("2"),
+        PhotoInformation("2"),
+        PhotoInformation("2"),
+        PhotoInformation("2"),
+        PhotoInformation("2"),
+        PhotoInformation("2"),
+        PhotoInformation("2"),
+        PhotoInformation("2"),
+        PhotoInformation("2"),
+        PhotoInformation("2"),
+        PhotoInformation("2"),
+        PhotoInformation("2"),
+        PhotoInformation("2"),
+        PhotoInformation("2"),
+        PhotoInformation("2"),
+        PhotoInformation("2"),
+        PhotoInformation("2"),
+        PhotoInformation("2"),
+        PhotoInformation("2"),
+        PhotoInformation("2"),
+        PhotoInformation("3")
+    )
 )
 
 @Stable
@@ -12,3 +69,8 @@ internal sealed interface TabType {
     data object TrashBin : TabType
     data object ToDeletion : TabType
 }
+
+@Stable
+internal data class PhotoInformation(
+    val uri: String
+)
